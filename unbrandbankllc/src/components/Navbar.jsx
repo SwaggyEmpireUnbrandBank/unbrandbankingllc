b6e33458eeb3768fc9084925d58668adf719ea37
unbrandbanking <unbrandbanking@gmail.com>
2023-08-26 16:36:22 -0400

    lllllllllllllllllllllllllllllll

---
+++ unbrandbankllc/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className='text-white'>
+            <h1>UnBrandBankLLC</h1>
+        </div>
+           
+    )
+}
+
+export default Navbar;
