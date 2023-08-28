e70103c39d05843c2262316e9a53f4a71aa6a9d4
unbrandbanking <unbrandbanking@gmail.com>
2023-08-28 13:38:40 -0400

    lllllllllllllll yup

---
+++ unbrandbankllc/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#03a9fc",
+        secondary: "#04db2f",
+        dimWhite: "#f7fbfc",
+        dimBlue: "#6941fa",
+      },
+      fontFamily: {
+        Merriweather: ["Merriweather", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
